5cda997c46da3c7f30e9e684f52e6334fbab785d
KyleHorton <Kyle.Hortonx6@gmail.com>
2019-04-30 22:52:06 -0400

    trying to angular

---
+++ static/controllers/checklistController.js
@@ -0,0 +1,18 @@
+qccApp.controller("ChecklistController", ["$scope", "$location", "$window", "$http", function ($scope, $location, $window, $http) {
+
+    $scope.loadChecklists = function(){
+        $http.get("/checklist")
+            .then(function (result) {
+                console.log(result.data);
+                $scope.checklist = result.data;
+
+            }, function (error) {
+                console.log(error);
+            });
+    };
+
+    $scope.$on('$viewContentLoaded', function(){
+        $scope.loadChecklists();
+        console.log("hi");
+    });
+}]);
