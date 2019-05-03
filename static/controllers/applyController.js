566293124e50d0b5907b0824aa6df7cf39d8d210
Mark Russo <mmrusso3@gmail.com>
2019-05-03 16:06:13 -0400

    dumb alert

--- static/controllers/applyController.js
+++ static/controllers/applyController.js
@@ -1,17 +1,17 @@
 qccApp.controller("ApplyController", ["$scope", "$location", "$window", "Auth", function ($scope, $location, $window, Auth) {
-    
+
     if(JSON.parse(sessionStorage.getItem("userInfo")) == null){
         $scope.adminBtns = false;
         $scope.approvedBtns = false;
         $scope.loggedIn = false;
-        
+
     }else{
         $scope.adminBtns = JSON.parse(sessionStorage.getItem("userInfo")).officer;
         $scope.approvedBtns = JSON.parse(sessionStorage.getItem("userInfo")).approved;
         $scope.loggedIn = true;
 
     }
-    
+
     $scope.sendApplication = function(){
         $scope.newUser = {
             name: $('#firstname').val() + ' ' + $('#lastname').val(),
@@ -22,15 +22,17 @@ qccApp.controller("ApplyController", ["$scope", "$location", "$window", "Auth", 
             officer: false
 
         }
+
         //console.log($scope.newUser);
         Auth.apply($scope.newUser)
         .then(function(result){
             console.log($window.sessionStorage["userInfo"]);
+            alert("Application has been submitted!")
             $location.path('/')
     }, function(error){
-        
+
     });
 
-        
+
     };
 }]);
